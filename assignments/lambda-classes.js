9ab67ca294c591e6e3c6410a60367cf910193d39
Duraan Ali <duraan522@gmail.com>
2019-06-13 11:30:36 -0500

    Added old code to project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,10 @@
 // CODE here for your Lambda Classes
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+});
